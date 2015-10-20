0080389558ef08c0f1cc5198cb759e5843bba617
hungyipu <bubupu@gmail.com>
2015-10-20 23:19:57 +0800

    Create task1.cpp

---
+++ src/task1.cpp
@@ -0,0 +1,16 @@
+/***********************************************************************************
+    Copyright 2015  Hung-Yi Pu, Kiyun Yun, Ziri Yonsi, Sunk-Jin Yoon
+                        Odyssey  version 1.0   (released  2015)
+    This file is part of Odyssey source code. Odyssey is a public, GPU-based code 
+    for General Relativistic Radiative Transfer (GRRT), following the 
+    ray-tracing algorithm presented in 
+    Fuerst, S. V., & Wu, K. 2007, A&A, 474, 55, 
+    and the radiative transfer formulation described in 
+    Younsi, Z., Wu, K., & Fuerst, S. V. 2012, A&A, 545, A13
+    Odyssey is distributed freely under the GNU general public license. 
+    You can redistribute it and/or modify it under the terms of the License
+        http://www.gnu.org/licenses/gpl.txt
+    The current distribution website is:
+	https://github.com/hungyipu/Odyssey/
+	
+***********************************************************************************/
